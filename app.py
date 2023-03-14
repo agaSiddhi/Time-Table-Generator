dae17c5da091a18e642a5e99c12d6afe6dc08255
ASK-03 <abhisheksinghkushwaha22@gmail.com>
2023-03-14 14:39:12 +0530

    Added book slot and clash check functions in app.py and time table html file

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ with open('slots.json') as f:
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html', courses=courses.keys())
 
 
 @app.route('/check_clash', methods=['GET', 'POST'])
@@ -38,6 +38,37 @@ def check_clash():
         book_slot(df=df, slots=slots, course_code=i, course_details=courses)
     print(df)
     return render_template('time_table.html', df=df)
+
+
+def book_slot(df, slots, course_code, course_details):
+    course_info=course_details[course_code]
+    slot=course_details[course_code]['slot']
+    if slot=='NA':
+        return
+    slot_data=slots.get(slot)
+    if slot_data==None:
+        print("slot not readable")
+        return 
+    cell_data=course_code + "/" + course_info['name'] + "/" + course_info['room'] + "/" + course_info['fractal']+" "
+    if len(slot_data)==2:
+        for i in range(len(slot_data)):
+            if type(df.loc[slot_data[i]["day"], slot_data[i]['start']+"-"+slot_data[i]['end']])==str:
+                df.loc[slot_data[i]["day"], slot_data[i]['start']+"-"+slot_data[i]['end']]+=cell_data
+            else:
+                df.loc[slot_data[i]["day"], slot_data[i]['start']+"-"+slot_data[i]['end']]=cell_data
+        
+    if len(slot_data)==1:
+        possi={'8:30-11:20' : ['9:50', '10:00'], '10:00-12:50' : ['11:20', '11:30'], '14:00-16:50' : ['15:20', '15:30'], '15:30-18:30' : ['16:50', '17:00']}
+        if slot_data[0]['start']+"-"+slot_data[0]['end'] in possi.keys():
+            mid=possi[slot_data[0]['start']+"-"+slot_data[0]['end']]
+            if type(df.loc[slot_data[0]["day"], slot_data[0]['start']+"-"+mid[0]])==str:
+                df.loc[slot_data[0]["day"], slot_data[0]['start']+"-"+mid[0]]+=cell_data
+                df.loc[slot_data[0]["day"], mid[1]+"-"+slot_data[0]['end']]+=cell_data
+            else:
+                df.loc[slot_data[0]["day"], slot_data[0]['start']+"-"+mid[0]]=cell_data
+                df.loc[slot_data[0]["day"], mid[1]+"-"+slot_data[0]['end']]=cell_data
+        else:
+            return 
 
 if __name__ == '__main__':
     app.run(debug=True)
